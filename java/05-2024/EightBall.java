d9c4ce0eec4ef1df694e5cb1a62e0b614489e313
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-05-12 19:59:30 -0500

    Update EightBall.java
    - still a work in progress java program
    
    Current Issues:
    The program stops after one question and refuses to loop for another question to be asked

--- java/05-2024/EightBall.java
+++ java/05-2024/EightBall.java
@@ -7,14 +7,27 @@ a Java version of the 8 Ball python file.
 import java.util.* ;
 
 public class EightBall {
-  public static void main(String [] args) {
-        
-  }
+	public static void main(String [] args) {
+		String eightBall = EightBallResponse() , userInput = "y" ;
+		if (userInput == "y" || userInput == "yes") {
+			System.out.print("Ask the Eight Ball a question? ") ;
+			Scanner input = new Scanner(System.in) ;
+			String question = input.nextLine() ; 
+			System.out.printf("\nThe Eight Ball's response to [%s] is %s" , question , eightBall) ;
 
-  public static String EightBallResponse() {
-    String[] responses = {"Yes" , "No" , "Maybe" , "Ask Again Later"} ;
-    Random r = new Random() ;
-    int responseIndex = r.nextInt(responses.length - 1)
-    return answer ;
-  }
+			System.out.printf("\nWould you like to ask another question? (yes/no) ") ;
+			Scanner input2 = new Scanner(System.in) ;
+			userInput = input2.nextLine() ;
+		} 
+		System.out.printf("Have a nice day!\n") ;
+
+		System.out.println(userInput) ;
+	}
+
+	public static String EightBallResponse() {
+		String[] responses = {"Yes" , "No" , "Maybe" , "Ask Again Later"} ;
+		Random r = new Random() ;
+		int i = r.nextInt(responses.length) ;
+		return responses[i] ;
+	}
 }
