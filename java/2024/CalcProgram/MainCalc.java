c80a71bccbf32d32d1ecf4ea6e8f0572e0798ca8
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-08-20 01:09:08 -0500

    Update MainCalc.java
    - renamed scanner variables :D 
    - added pythagorean theorem calculator

--- java/2024/CalcProgram/MainCalc.java
+++ java/2024/CalcProgram/MainCalc.java
@@ -7,27 +7,51 @@ public class MainCalc {
 
         System.out.println("Which calculator do you need?") ;
         System.out.print("[Bas]ic, [Pyt]agorean: ") ;
-        Scanner input = new Scanner(System.in) ;
-        calcType = input.nextLine() ;
-
-        Scanner inpu = new Scanner(System.in) ;
-        System.out.print("1st Value: ") ;
-        b = inpu.nextDouble() ;
-        Scanner inp = new Scanner(System.in) ;
-        System.out.print("2nd Value: ") ;
-        c = inp.nextDouble() ;
+        Scanner yotsuba = new Scanner(System.in) ;
+        calcType = yotsuba.nextLine() ;
 
         switch(calcType) {
             case "bas" :
-                Scanner inputA = new Scanner(System.in) ;
-                System.out.print("Opperations [+ , - , * , /]: ") ;
-                a = inputA.next() ; 
+                Scanner aqua = new Scanner(System.in) ;
+                System.out.print("1st Value: ") ;
+                b = aqua.nextDouble() ;
+                Scanner darkness = new Scanner(System.in) ;
+                System.out.print("2nd Value: ") ;
+                c = darkness.nextDouble() ;
 
-                BasicCalc tempCalc = new BasicCalc(a , b , c) ;
-                System.out.printf("%.3f %s %.3f = %.3f\n" , b , a , c ,tempCalc.getAnswer()) ;
+                Scanner nino = new Scanner(System.in) ;
+                System.out.print("Opperations [+ , - , * , /]: ") ;
+                a = nino.next() ; 
+
+                BasicCalc basCalc = new BasicCalc(a , b , c) ;
+                System.out.printf("%.3f %s %.3f = %.3f\n" , b , a , c ,basCalc.getAnswer()) ;
                 break ;
             case "pyt" :
-                System.out.println("PYTHAOGREAN") ;
+                Scanner miku = new Scanner (System.in) ;
+                System.out.print("What side is missing? [a , b , c]: ") ;
+                a = miku.next() ; 
+
+                if(a != "c"){
+                    Scanner futaba = new Scanner(System.in) ;
+                    System.out.print("Hypotenuse: ") ;
+                    b = futaba.nextDouble() ;
+                    Scanner mai = new Scanner(System.in) ;
+                    System.out.print("Leg: ") ;
+                    c = mai.nextDouble() ;
+
+                    PythaCalc pytCalc = new PythaCalc(a , b , c) ;
+                    System.out.printf("The missing leg = %.3f\n" , pytCalc.getAnswer()) ;
+                } else {
+                    Scanner nami = new Scanner(System.in) ;
+                    System.out.print("Leg: ") ;
+                    b = nami.nextDouble() ;
+                    Scanner itsuki = new Scanner(System.in) ;
+                    System.out.print("Leg: ") ;
+                    c = itsuki.nextDouble() ;
+
+                    PythaCalc pytCalc = new PythaCalc(a , b , c) ;
+                    System.out.printf("The hypotenuse = %.3f\n" , pytCalc.getAnswer()) ;                   
+                }
                 break ;
         }
     }
