e7a98af08689bfcf66b9ebd586965868252eceeb
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-10-13 20:47:26 -0500

    Update Vehicle.py
    - IT WORKS

--- python/2024/Lab1/Vehicle.py
+++ python/2024/Lab1/Vehicle.py
@@ -1,24 +1,20 @@
+## 10/13/24
 ## Rewriting my Csci 112 - Lab 1 classes / program in python
-class Vehicle:
-        def __init__(self, color: str , make: str , model: str , cost: float):
+class Vehicle (object):
+        def __init__(self, color: str , make: str , model: str , cost: float) -> None:
                 # Equivalent to a parameterized constructor in java
                 self.color = color
                 self.make = make
                 self.model = model
                 self.cost = cost
 
-        def __str__(self):
+        def get_cost(self) -> float:
+                return self.cost
+
+        def __str__(self) -> str:
                 # Equivalent to a toString() function in java
                 return f"A {self.color} {self.make} {self.model} costs ${self.cost}"
 
-def main():
-        honda = Vehicle('Blue', 'Honda', 'Civic', 24999.99)
-        jeep = Vehicle('Marron', 'Jeep', 'Cherokke', 9999.99)
-        blank = Vehicle('' , '' , '' , 0)
-        ## Similar to the main function in java
-        print(honda)
-        print(jeep)
-        print(blank)
-
-if __name__ == '__main__':
-        main()
+        def __repr__(self) -> str:
+                return (f'Vehicle(color="{self.color}" make="{self.make}" model="{self.model}"'
+                        f' cost="{self.cost}")')
