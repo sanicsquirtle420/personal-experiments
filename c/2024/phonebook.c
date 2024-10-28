113b264ac1b77d7e8f963adefed2a53f85698882
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-10-27 23:49:57 -0500

    Update phonebook.c
    fixed my issues with core dumps
    - instead of using char* from the user input I used an array of characters (because char* was giving me memory issues)
    - added some more people :D

--- c/2024/phonebook.c
+++ c/2024/phonebook.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
-
+#include <string.h>
+                       
 typedef struct
 {
-    char name ;
-    int id ;
-} person ;
+    char *name ;    // sense strings don't exist in C they can be defined as
+    int id ;        // char[] or char* or you can make them exist by using
+} person ;          // typedef char *string ; 
 
 int main(void)
 {
-    person people[3] ;
-    char target ;
+    person people[5] ;
+    char search[20] ;
 
-    people[0].name = 'J' ;
+    people[0].name = "Juno" ;
     people[0].id = 1154541 ; 
-    people[1].name = 'K' ;
+    people[1].name = "Kiriko" ;
     people[1].id = 1167428 ;
-    people[2].name = 'D' ;
+    people[2].name = "D.va" ;
     people[2].id = 1189524 ;
+    people[3].name = "Winton" ;
+    people[3].id = 1087534 ;
+    people[4].name = "Mei" ;
+    people[4].id = 1096752 ;
 
-    printf("Key: [J]uno , [K]irko , [D].va\n") ;
     printf("\tWho are you looking for? ") ;
-    scanf("%c" , &target) ;
+    scanf("%19s" , search) ; // sets the limit to 19 characters
 
-    for(int i = 0 ; i < 3 ; i++)
+    for(int i = 0 ; i < 5 ; i++)
     {
-        if(people[i].name == target)
+        if(strcmp(people[i].name , search) == 0)
         {
             printf("\tFound! their ID number is %d\n" , people[i].id) ;
             return 0 ;
@@ -34,4 +38,3 @@ int main(void)
     printf("\tNot found :(\n") ;
     return 1 ;
 }
-
