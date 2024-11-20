e755c1bb953893fdf98c36043b223d692016926a
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-11-19 20:16:40 -0600

    Update Circle.kt
    - added toString()
    - added constructors

--- kotlin/2024/Circle.kt
+++ kotlin/2024/Circle.kt
@@ -1,5 +1,7 @@
-class Circle {
-    private var radius: Double = 0.0
+class Circle(rad: Double) {
+    private var radius: Double = rad
+
+    constructor(): this(0.0)
 
     fun setRadius(r: Double): Unit {
         radius = r
@@ -19,5 +21,10 @@ class Circle {
 
     fun getCircumference(): Double {
         return 2 * radius * Math.PI
+    }
+
+    override fun toString(): String {
+        return String.format("Area: %.3f, Diameter: %.3f, Circumference: %.3f" ,
+            getArea() , getDiameter() , getCircumference())
     }
 }
