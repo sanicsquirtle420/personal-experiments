ee0c341ef0d0fd3be8e8f5cd2e432b9ed8999dc9
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-12-01 11:55:15 -0600

    Update calc.c
    - loops until you stop it with 'n' 
    - division formats to 0 decimal places if x % y = 0

--- c/2024/calc.c
+++ c/2024/calc.c
@@ -8,18 +8,53 @@ float div(int a , float b) ;
 int main(void)
 {
     int x , y ;
+    char response , opperation ;
 
     printf("--- C Calculator ---\n") ;
 
-    printf("What's x? ") ;
-	  scanf("%d" , &x) ;
-	  printf("What's y? ") ;
-	  scanf("%d" , &y) ;
+    do
+    {
+        printf("Select an opperation [+ , - , * , /]: ") ;
+        scanf(" %c" ,  &opperation) ;
+        // the space in the front will clear the buffer in scanf
 
-    printf("\t%i + %i = %i\n" , x , y , add(x , y)) ;
-    printf("\t%i - %i = %i\n" , x , y , sub(x , y)) ;
-    printf("\t%i * %i = %i\n" , x , y , mul(x , y)) ;
-    printf("\t%i / %i = %.5f\n" , x , y , div(x , y)) ;
+        printf("What's x? ") ;
+        scanf("%d" , &x) ;
+        printf("What's y? ") ;
+        scanf("%d" , &y) ;
+
+        if (opperation == '+')
+        {
+            printf("Ans:\t%i + %i = %i\n" , x , y , add(x , y)) ;
+        }
+        else if (opperation == '-')
+        {
+            printf("Ans:\t%i - %i = %i\n" , x , y , sub(x , y)) ;
+        }
+        else if (opperation == '*')
+        {
+            printf("Ans:\t%i * %i = %i\n" , x , y , mul(x , y)) ;
+        }
+        else if (opperation == '/')
+        {
+            if (x % y == 0) 
+            {   // decision structor to determin if there is no remainder
+                printf("Ans:\t%i / %i = %.0f\n" , x , y , div(x , y)) ;
+            }
+            else
+            {   // formats the float to 5 decimal places
+                printf("Ans:\t%i / %i = %.5f\n" , x , y , div(x , y)) ;
+            }
+        }
+        else
+        {
+            printf("Invalid opperation.\n") ;
+            return 0 ;
+        }
+
+        printf("Would you like to continue? (y/n): ") ;
+        scanf(" %c" , &response) ;
+    } while (response != 'n') ;
 
     return 0 ;
 }
