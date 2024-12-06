300799a53b5c45efe0e152211d757c4589367236
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-12-06 00:21:46 -0600

    Upload rac_math header files
    - moved the functions into a header file

---
+++ c/2024/rac_math.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "rac_math.h"
+
+int add(int a , int b) 
+{
+    return a + b ;
+}
+
+int sub(int a , int b)
+{
+    return a - b ;
+}
+
+int mul(int a , int b)
+{
+    return a * b ;
+}
+
+float div(int a , float b)
+{
+    return a / b ;
+}
---
+++ c/2024/rac_math.h
@@ -0,0 +1,10 @@
+#ifndef RAC_MATH_H
+#define RAC_MATH_H
+
+// functions
+int add(int a , int b) ;
+int sub(int a , int b) ;
+int mul(int a , int b) ;
+float div(int a , float b) ;
+
+#endif
