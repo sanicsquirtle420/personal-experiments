1ed0b426fd36aa90136f8c82c72da94d3373bff2
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-12-17 17:43:35 -0600

    Update calc.cpp
    - added factorials
    - created RacMath.h & RacMath.cpp to hold the functions
    
    TODO:
    factorials: current output "6 ! 0 = 720"
    figure out how to format strings in C++, so I can get rid of the 0 placeholder

--- c++/2024/calc.cpp
+++ c++/2024/calc.cpp
@@ -1,65 +1,50 @@
 #include <iostream>
+#include "RacMath.h"
 
 using namespace std;
 
-int add(int a , int b) ;
-int sub(int a , int b) ;
-int mul(int a , int b) ;
-float div(int a , float b) ;
-
 int main() {
-    int num1 , num2 ;
+    int num1 ;
     float ans ;
-    char opp ;
+    char opp , response ;
+    RacMath math ;
 
-    cout << "Choose your opperation [+ , - , * , /]: " ;
-    cin >> opp ;
-    
-    if(opp != '+' && opp != '-' && opp != '*' && opp != '/') {
-	    cout << "Invalid opperation.\n" ;
-	    return 1 ;
+    cout << "--- C++ Calculator ---" << endl ;
+    do {
+    	cout << "Choose your opperation [+ , - , * , / , !]: " ;
+    	cin >> opp ;
+
+    	switch(opp){
+		case '+':
+			ans = math.add() ;
+			break ;
+        	case '-':
+            	ans = math.subtract() ;
+            	break ;
+        	case '*':
+            	ans = math.multiply() ;
+            	break ;
+        	case '/':
+        		ans = math.divide() ;
+        		break ;
+		case '!':
+	    	cout << "Enter a number: " ;
+	    	cin >> num1 ;
+
+	    	ans = math.factorial(num1) ;
+	    	math.setSecond(0) ;
+	    	math.setFirst(num1) ; 
+	    	break ;
+		default:
+	    	cout << "Invalid opperation" << endl ;
+	    	return 0 ;
     }
 
-    cout << "Enter your first value: " ;
-    cin >> num1 ;
-    cout << "Enter your second value: " ;
-    cin >> num2 ;
+    cout.precision(5) ;
+    cout << math.getFirst() << " " << opp << " " <<  math.getSecond() << " = " << ans << endl ;
+    cout << "Would you like to continue (y/n)? " ;
+    cin >> response ;
+    } while(response != 'n') ;
 
-    switch(opp){
-        case '+':
-            ans = add(num1 , num2) ;
-            break ;
-        case '-':
-            ans = sub(num1 , num2) ;
-            break ;
-        case '*':
-            ans = mul(num1 , num2) ;
-            break ;
-        case '/':
-	        ans = div(num1 , (float) num2) ;
-	        break ;
-    }
-
-    cout.precision(3) ;
-    cout << num1 << opp << num2 << "=" << ans  << "\n" ;
-    /* I hate this so much just let me format a string
-       I tried format() but it looks ugly and didn't work which 
-       I know is a skill issue on my part */
     return 0 ;
 } 
-
-int add(int a , int b) {
-	return a + b ;
-}
-
-int sub(int a , int b) {
-	return a - b ;
-}
-
-int mul(int a , int b) {
-	return a * b ;
-}
-
-float div(int a , float b) {
-	return a / b ;
-}
