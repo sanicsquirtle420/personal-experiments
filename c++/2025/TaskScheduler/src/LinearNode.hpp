c9bda78a6b2d5f753eb1e9d6a559fee4ccb9d709
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2025-03-11 19:39:49 -0500

    Update LinearNode.hpp
    - added definitions to .hpp file
    * to fix unreferenced method errors

--- c++/2025/TaskScheduler/src/LinearNode.hpp
+++ c++/2025/TaskScheduler/src/LinearNode.hpp
@@ -1,6 +1,8 @@
 #ifndef LINEARNODE_HPP
 #define LINEARNODE_HPP
+#include <iostream>
 
+using namespace std ;
 template <typename T>
 
 class LinearNode {
@@ -13,5 +15,23 @@ class LinearNode {
         LinearNode<T>* getNext() ;
         void setNext(LinearNode<T>* node) ;
 } ;
+
+template <typename T>
+LinearNode<T> :: LinearNode(T elem) : element(elem) , next(nullptr) {}
+
+template <typename T>
+T LinearNode<T> :: getElement() {
+    return element ;
+}
+
+template <typename T>
+LinearNode<T>* LinearNode<T> :: getNext() {
+    return next ;
+}
+
+template <typename T>
+void LinearNode<T> :: setNext(LinearNode<T>* node) {
+    next = node ;
+}
 
 #endif
