3d9346b69e9e75613f782c6e0a9a6c67a4ac4e10
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2025-03-13 00:51:55 -0500

    Update main.cpp
    - added decision structures / logic

--- c++/2025/TaskScheduler/main.cpp
+++ c++/2025/TaskScheduler/main.cpp
@@ -1,39 +1,75 @@
 #include "src/Queue.hpp"
-#include <string>
 
 using namespace std ;
 
 int main(void) {
-    Queue<int> pri ;
-    Queue<int> reg ;
+    Queue<string> pri ;
+    Queue<string> reg ;
     bool stop = true ;
 
     cout << "Welcome to the Task Manager!" << endl ;
     while(stop){
         char option = ' ' ;
-        cout << "Options:\np: Priority task , r: Regular task" << endl ;
-        cout << "s: See tasks , e: Exit" << endl ;
+        cout << "Options:\n1: Add tasks , 2: Remove tasks , 3: See tasks , 0: Exit" << endl ;
         cin >> option ;
 
-        if(option == 'p') {
-            int a = 0 ;
-            cout << "Enter the task " ;
-            cin >> a ;
-            pri.enqueue(a) ;
-            cout << a << " added to priority!" << endl ;
+        if(option == '1') {
+            option = ' ' ;
+            cout << "\tOptions: p: Priority , r: Regular" << endl ;
+            cout << "\tAdding to... " ;
+            cin >> option ;
+
+            if(option == 'p') {
+                char input[100] ; // character limit: 100
+                cout << "\tEnter the task: " ;
+                cin.clear() ; // clearing the buffer
+                cin.ignore(10000 , '\n') ;
+                cin.getline(input , sizeof(input)) ; // accepts spaces
+                pri.enqueue(input) ;
+                cout << "\"" << input << "\" added to priority!" << endl ;
+            } else { 
+                // defaults to regular queue
+                char input[100] ; 
+                cout << "\tEnter the task: " ;
+                cin.clear() ; 
+                cin.ignore(10000 , '\n') ;
+                cin.getline(input , sizeof(input)) ;
+                reg.enqueue(input) ;
+                cout << "\"" << input << "\" added to regular!" << endl ;
+            }
         } 
-        else if(option == 'r') {
-            int a = 0 ;
-            cout << "Enter the task " ;
-            cin >> a ;
-            reg.enqueue(a) ;
-            cout << a << " added to regular!" << endl ;
+        else if(option == '2') {
+            option = ' ' ;
+            cout << "\tOptions: p: Priority , r: Regular" << endl ;
+            cout << "\tRemoving from... " ;
+            cin >> option ;
+
+            if(option == 'p') {
+                cout << "\"" << pri.dequeue() << "\" was removed from priority." << endl ;
+            } else { 
+                // defaults to regular queue
+                cout << "\"" << reg.dequeue() << "\" was removed from priority." << endl ;
+            }
         }
-        else if(option == 's') {
-            Queue<int> total ;
-            cout << "Tasks:\n" << total.toString() << endl ;
+        else if(option == '3') {
+            option = ' ' ;
+            cout << "\tOptions: p: Priority , r: Regular , a: All" << endl ;
+            cout << "\tLooking at... " ;
+            cin >> option ;
+
+            if(option == 'p') {
+                cout << "[!!!] Priority tasks:\n" << pri.toString() << endl ;
+            } 
+            else if(option == 'r'){
+                cout << "Regular tasks:\n" << reg.toString() << endl ;
+            } else { 
+                // default will display all tasks
+                Queue<string> total = pri.merge(reg);
+                cout << "ALL Tasks:\n" << total.toString() << endl ;
+            }
+            
         }
-        else if(option == 'e') {
+        else if(option == '0') {
             stop = false ;
         } 
         else {
